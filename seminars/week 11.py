272553a7b2cd52f0ed49937144381e1d341526df
c3683414 <f.choudhury2382@student.leedsbeckett.ac.uk>
2025-12-01 14:12:32 +0000

    :(

---
+++ seminars/week 11.py
@@ -0,0 +1,20 @@
+from keras.datasets.mnist import load_data
+# load the images into memory
+(trainX, trainy), (testX, testy) = load_data()
+#summarize the shape of the dataset
+print('Train', trainX.shape, trainy.shape)
+print('Test', testX.shape, testy.shape)
+
+# example of loading the mnist dataset from
+keras.datasets.mnist import load_data from matplotlib import pyplot
+# load the images into memory
+(trainX, trainy), (testX, testy) = load_data()
+# plot images from the training dataset
+for i in range(25):
+    # define subplot
+    pyplot.subplot(5, 5, 1 + i)
+    #turn off axis
+    # pyplot.axis('off')
+ plot raw pixel data
+pyplot.imshow(trainX[i], cmap='gray_r')
+pyplot.show()
